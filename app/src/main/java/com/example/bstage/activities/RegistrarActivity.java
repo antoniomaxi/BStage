7acc48ad7be157261d81e33a1f016e36d6ed0601
richardeascanio <rascanio97@gmail.com>
2018-11-28 20:59:08 -0400

    listo subir imagenes a storage (perfil)

--- app/src/main/java/com/example/bstage/activities/RegistrarActivity.java
+++ app/src/main/java/com/example/bstage/activities/RegistrarActivity.java
@@ -1,16 +1,30 @@
 package com.example.bstage.activities;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import com.example.bstage.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,12 +38,24 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.UUID;
+
+import retrofit2.http.Url;
 
 
 public class RegistrarActivity extends AppCompatActivity {
 
     Button btnSignUp;
     EditText nombre, username, email, password;
+    ImageView ImgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESCODE = 1;
+    Uri pickedImgUri;
+
+    //Firebase
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +67,25 @@ public class RegistrarActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
+        ImgUserPhoto = findViewById(R.id.reqUserPhoto);
+
+        //firebase
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(Build.VERSION.SDK_INT >= 22){
+
+                    checkAndRequestForPremission();
+                }
+                else{
+                    openGallery();
+                }
+            }
+        });
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +93,74 @@ public class RegistrarActivity extends AppCompatActivity {
 
                 //make POST request
                 new PostDataTask().execute("http://backstage-backend.herokuapp.com/api/usuarios");
-
-                Intent i = new Intent(RegistrarActivity.this, IniciarSesionActivity.class);
+                uploadImage();
+                Intent i = new Intent(RegistrarActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
 
+    }
+
+    private void uploadImage() {
+
+        StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+
+        ref.putFile(pickedImgUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //Upload succesful
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegistrarActivity.this, "La imagen no se pudo subir "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void openGallery() {
+
+        //TODO: open gallery intent and wait for user to pick an image
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESCODE);
+    }
+
+    private void checkAndRequestForPremission() {
+
+        if(ContextCompat.checkSelfPermission(RegistrarActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                !=PackageManager.PERMISSION_GRANTED){
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegistrarActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                Toast.makeText(RegistrarActivity.this, "Por favor acepta los permisos requeridos", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(RegistrarActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+            }
+        }
+        else{
+            openGallery();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if((resultCode == RESULT_OK) && (requestCode == REQUESCODE) && (data != null)){
+
+            //The user has successfully picked an image
+
+            //We need to save its reference to a Uri variable
+
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
+        }
     }
 
     class PostDataTask extends AsyncTask<String, Void, String> {
@@ -88,6 +195,8 @@ public class RegistrarActivity extends AppCompatActivity {
             if (progressDialog != null) {
                 progressDialog.dismiss();
             }
+
+
         }
 
         private String postData(String urlPath) throws IOException, JSONException {
@@ -103,6 +212,7 @@ public class RegistrarActivity extends AppCompatActivity {
                 dataToSend.put("Username", username.getText().toString());
                 dataToSend.put("Correo", email.getText().toString());
                 dataToSend.put("Clave", password.getText().toString());
+                dataToSend.put("Admin", false);
 
                 //Initialize and config request, then connect to server.
                 URL url = new URL(urlPath);
